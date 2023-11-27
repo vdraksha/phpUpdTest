d87b076661ad96b5a3c54ba06404a8c8cfb3aaee
vdraksha <vdraksha@yandex.ru>
2023-11-27 23:33:04 +0500

    1

--- main.py
+++ main.py
@@ -7,70 +7,83 @@ import requests
 DATA = dict()
 
 
-def authentication(domain, login=None, password=None):
-    """Создает сессию и выполняет аутентификацию, если потребуется.
-    :param domain: ...
-    :param login: None, если аутентификация не требуется.
-    :param password: None, если аутентификация не требуется.
-    :return: Возвращает сессию.
-    """
-    session = requests.Session()
-    session.verify = False
-    if password is None:
-        return session
-    else:
-        session.auth = (login, password)
-        session.post(domain)
-        return session
-
-
-def parsed_endpoint(response):
-    """Собирает ендпоинты для внутренних ссылок.
-    :param response: Ответ на запрос, из которого парсим документ.
-    :return: Возвращает список эндпоинтов.
-    """
-    path_list = list()
-    document = BeautifulSoup(response.text, "html.parser")
-    for data in document.find_all('a'):
-        if data["href"].startswith("/"):
-            path_list.append(data["href"])
-    return path_list
-
-
-def parsed_text(response):
-    """Ищет на странице текст с окончание .php.
-    :param response: ...
-    :return: True, если есть .php на странице. Иначе False.
-    """
-    document = BeautifulSoup(response.text, "html.parser")
-    return True if '.php' in document.get_text() else False
-
-
-def collect(session, domain, lt=None, i=0):
-    """Рекурсивно перебирает все внутренние ссылки.
-    :param session: ...
-    :param domain: ...
-    :param lt: Список ендпоинтов.
-    :param i: Индекс для lt.
-    :return: Вовзращает словарь со ссылками, статус кодами, флагом наличия ошибок.
-    """
-    if lt is None:
-        lt = [""]
-
+def clear_and_status():
     system("cls")
     print("Собрано:", len(DATA))
 
-    if i > len(lt):
+
+def create_session():
+    """Создает сессию для хранения пользовательских данных.
+    :return: Созданная сессия.
+    """
+    session = requests.Session()
+    session.verify = False
+    return session
+
+
+def auth(session, domain, login, password):
+    """Отправляет логин и пароль для базовой аутентификации.
+    :param session: Cессия для хранения пользовательских данных.
+    :param domain: Домен, с которым работает скрипт.
+    :param login: Логин для аутентификации.
+    :param password: Пароль для аутентификации.
+    :return: Сессия с пройденной аутентификацией.
+    """
+    session.auth = (login, password)
+    session.post(domain)
+    return session
+
+
+def parsed_path(document):
+    """Просматривает html-документ из ответа и собирает все пути в список.
+    :param document: Полученный html-документ.
+    :return: Список путей.
+    """
+    path_list = list()
+    for data in document.find_all('a'):
+        path_link = data["href"]
+        if path_link.startswith("/") and path_link not in path_list:
+            path_list.append(path_link)
+    return path_list
+
+
+def parsed_text(document):
+    """Ищет указанный текст на странице.
+    :param document: Полученный html-документ.
+    :return: True, если есть .php на странице. Иначе False.
+    """
+    return True if '.php' in document.get_text() else False
+
+
+def collect(session, domain, path_list=None, i=0):
+    """Рекурсивно перебирает все внутренние ссылки.
+    :param session: Cессия для хранения пользовательских данных.
+    :param domain: Домен, с которым работает скрипт.
+    :param path_list: Список путей.
+    :param i: Индекс path_list для выхода из рекурсии.
+    :return: Словарь со ссылками, статус кодами, флагом наличия ошибок.
+    """
+    if path_list is None:
+        path_list = [""]
+
+    if i == len(path_list):
         return DATA
 
-    if domain + lt[i] not in DATA:
-        response = session.get(domain + lt[i])
-        DATA[domain + lt[i]] = [response.status_code]
-        DATA[domain + lt[i]].append(parsed_text(response))
-        lt = parsed_endpoint(response)
-        return collect(session, domain, lt, i + 1)
+    # clear_and_status()
+
+    url = domain + path_list[i]
+    if url not in DATA:
+        response = session.get(url)
+        if response.status_code == 401:
+            login = input("Введите логин:")
+            password = input("Введите логин:")
+            session = auth(session, domain, login, password)
+            response = session.get(url)
+        document = BeautifulSoup(response.text, "html.parser")
+        DATA[url] = [response.status_code, parsed_text(document)]
+        return collect(session, domain, parsed_path(document))
     else:
-        return collect(session, domain, lt, i + 1)
+        return collect(session, domain, path_list, i + 1)
 
 
 def output_csv(data):
@@ -88,22 +101,9 @@ def output_csv(data):
 
 def interface():
     """Интерфейс для управления.
-    :return: ...
     """
     domain = input("Введите URL (http(s)://example.ru/):")
-
-    title = "Нужна аутентификация?"
-    options = ["Да", "Нет"]
-    option, index = pick(options, title)
-
-    if index == 0:
-        login = input("login:")
-        password = input("password:")
-        session = authentication(domain, login, password)
-    else:
-        session = authentication(domain)
-
-    collect(session, domain)
+    collect(create_session(), domain)
 
     output_csv(DATA)
     print(f"Собрано: {len(DATA)}. Данные выведены в {getcwd()}")
